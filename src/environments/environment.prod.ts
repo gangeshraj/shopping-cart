46137a1016fbe1b1a56d582ee17e4422a7b8793c
Gangesh Raj Sharma <https://github.com/gangeshraj>
2019-06-22 16:01:41 +0530

    my first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC4HRcxR3Ya_joXa1TheyRp3yDebx2I8Jk",
+    authDomain: "oshop-52398.firebaseapp.com",
+    databaseURL: "https://oshop-52398.firebaseio.com",
+    projectId: "oshop-52398",
+    storageBucket: "",
+    messagingSenderId: "804221064725",
+    appId: "1:804221064725:web:1719916003e02ada"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC4HRcxR3Ya_joXa1TheyRp3yDebx2I8Jk",
+    authDomain: "oshop-52398.firebaseapp.com",
+    databaseURL: "https://oshop-52398.firebaseio.com",
+    projectId: "oshop-52398",
+    storageBucket: "",
+    messagingSenderId: "804221064725",
+    appId: "1:804221064725:web:1719916003e02ada"
+  }
+};
